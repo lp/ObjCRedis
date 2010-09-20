406558480a5285959925f538df5db369dd87960a
Louis-Philippe Perron <lp@spiralix.org>
2010-09-20 10:14:41 -0400

    Basic framework skeleton

---
+++ ObjCRedis/ObjCRedis.h
@@ -0,0 +1,15 @@
+//
+//  ObjCRedis.h
+//  ObjCRedis
+//
+//  Created by Louis-Philippe on 10-09-20.
+//  Copyright 2010 Modul. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ObjCRedis : NSObject {
+
+}
+
+@end
---
+++ ObjCRedis/ObjCRedis.m
@@ -0,0 +1,14 @@
+//
+//  ObjCRedis.m
+//  ObjCRedis
+//
+//  Created by Louis-Philippe on 10-09-20.
+//  Copyright 2010 Modul. All rights reserved.
+//
+
+#import "ObjCRedis.h"
+
+
+@implementation ObjCRedis
+
+@end
