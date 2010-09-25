6a917d99a0c504edf67ee31d916fdb15fc51a33d
Louis-Philippe Perron <lp@spiralix.org>
2010-09-25 01:14:22 -0400

    NSArray includes: extension

---
+++ ObjCRedis/NSArray+includesHelpers.h
@@ -0,0 +1,15 @@
+//
+//  NSArray+includesHelpers.h
+//  ObjCRedis
+//
+//  Created by Louis-Philippe on 10-09-25.
+//  Copyright 2010 Modul. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (includesHelpers)
+
+- (NSNumber*)includes:(NSArray*)items;
+
+@end
---
+++ ObjCRedis/NSArray+includesHelpers.m
@@ -0,0 +1,24 @@
+//
+//  NSArray+includesHelpers.m
+//  ObjCRedis
+//
+//  Created by Louis-Philippe on 10-09-25.
+//  Copyright 2010 Modul. All rights reserved.
+//
+
+#import "NSArray+includesHelpers.h"
+
+@implementation NSArray (includesHelpers)
+
+- (NSNumber*)includes:(NSArray*)items {
+	BOOL (^mustSatisfy)(id obj, NSUInteger idx, BOOL *stop);
+	mustSatisfy = ^ (id obj, NSUInteger idx, BOOL *stop) {
+		return [items containsObject:obj];
+	};
+	
+	if ([[items indexesOfObjectsPassingTest:mustSatisfy] count] > 0) {
+		return [NSNumber numberWithBool:YES]; }
+	else { return [NSNumber numberWithBool:NO]; }
+}
+
+@end
