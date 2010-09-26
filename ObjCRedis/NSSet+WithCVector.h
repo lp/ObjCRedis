d17f28998d473cb7e2b5c0c7b8244a08ecf3c4fd
Louis-Philippe Perron <lp@spiralix.org>
2010-09-26 12:37:39 -0400

    NSSet extension to handle cVectors

---
+++ ObjCRedis/NSSet+WithCVector.h
@@ -0,0 +1,16 @@
+//
+//  NSSet+WithCVector.h
+//  ObjCRedis
+//
+//  Created by Louis-Philippe on 10-09-26.
+//  Copyright 2010 Modul. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSSet (WithCVector)
+
++ (NSSet*)setWithCVector:(char**)vec ofSize:(int)size;
+- (const char**)cVector;
+
+@end
---
+++ ObjCRedis/NSSet+WithCVector.m
@@ -0,0 +1,25 @@
+//
+//  NSSet+WithCVector.m
+//  ObjCRedis
+//
+//  Created by Louis-Philippe on 10-09-26.
+//  Copyright 2010 Modul. All rights reserved.
+//
+
+#import "NSSet+WithCVector.h"
+#import "NSArray+WithCVector.h"
+
+@implementation NSSet (WithCVector)
+
++ (NSSet*)setWithCVector:(char**)vec ofSize:(int)size
+{
+	return [NSSet setWithArray:[NSArray arrayWithCVector:vec ofSize:size]];
+}
+
+- (const char**)cVector
+{	
+	return [[self allObjects] cVector];
+}
+
+
+@end
